d4c946a6f2fe17727d0381a9413b7dd8ccdff786
Anudit Nagar <nagaranudit@gmail.com>
2020-04-13 22:30:51 +0530

    Integrate Fiat on/off ramping.

--- public/js/start/start.js
+++ public/js/start/start.js
@@ -1,5 +1,15 @@
+setInterval(()=>{
+    try {
+        document.getElementsByTagName('pre')[0].remove();
+    } catch (error) {
+
+    }
+}, 2000)
 window.addEventListener('load', async () => {
     document.getElementById("getStarted").addEventListener("click", gotoSwapPage);
+
+
+
 });
 
 function gotoSwapPage(){
@@ -22,3 +32,32 @@ function gotoSwapPage(){
         window.location.href = '/eos-x';
     }
 }
+
+
+
+function launchTransak() {
+    let transak = new TransakSDK.default({
+      apiKey: '13e83ebf-5430-4018-aae3-b16d63d83701',
+      environment: 'STAGING',
+      defaultCryptoCurrency: 'ETH',
+      walletAddress: '',
+      themeColor: '#ffffff',
+      fiatCurrency: 'INR',
+      email: '',
+      redirectURL: '/',
+      hostURL: window.location.origin,
+      widgetHeight: '600px',
+      widgetWidth: '100%',
+      exchangeScreenTitle:'AtomiX Network'
+    });
+    transak.init();
+    transak
+      .on(transak.ALL_EVENTS, (data) => {
+        console.log(data);
+      });
+    transak.on(transak.EVENTS.TRANSAK_ORDER_SUCCESSFUL, (orderData) => {
+      console.log(orderData);
+      alert(orderData);
+      transak.close();
+    });
+  }
